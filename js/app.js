06d7c043c7c0d63708f4b87dd80748dbca4f6447
Garan Guillory <garanguillory@gmail.com>
2015-02-24 10:31:38 -0600

    Add more animation & On-page Instructions

--- js/app.js
+++ js/app.js
@@ -8,15 +8,44 @@ $(document).ready(function(){
     $('.ryu-ready').hide();
   })
   .mousedown(function() {
-    // play hadouken sound
+    // playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show();
-    // animate hadouken to the right of the screen
+    $('.hadouken').finish().show().animate(
+      {'left': '300px'},
+      500,
+      function(){
+        $(this).hide();
+        $(this).css('left', '-212px');
+      }
+    );
   })
   .mouseup(function(){
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
-  });
+  })
 
 });
+
+$(document).keydown(function(e){
+  if(e.keyCode == 88){
+    $('.ryu-cool').show();
+    $('.ryu-ready').hide();
+    $('.ryu-still').hide();
+  }
+})
+
+.keyup(function(e){
+  if(e.keyCode == 88){
+    $('.ryu-cool').hide();
+    $('.ryu-ready').hide();
+    $('.ryu-still').show(); 
+  }
+});
+
+
+// function playHadouken () {
+//   $('#hadouken-sound')[0].volume = 0.5;
+//   $('#hadouken-sound')[0].load();
+//   $('#hadouken-sound')[0].play();
+// };
